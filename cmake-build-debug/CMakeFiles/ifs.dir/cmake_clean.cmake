e2601ad7725413cd87c6fb18835fd453ccb5a5e1
any-stone <kuxa.business@gmail.com>
2021-08-11 18:58:31 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ifs.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ifs.dir/main.cpp.o"
+  "ifs"
+  "ifs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ifs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
